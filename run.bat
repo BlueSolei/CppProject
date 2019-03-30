0f9e022aef216bfd69a792aef26c6ad527f4edaa
Shaul Fridman <shaul.fridman@gmail.com>
2019-03-30 17:22:38 +0300

    (+) change cmd param from 'test' --> 'tests'

--- run.bat
+++ run.bat
@@ -1,4 +1,4 @@
 pushd %~dp0
 cd build\bin
-if "%1"=="test" (DummyTest.exe) else (Dummy.exe)
+if "%1"=="tests" (DummyTest.exe) else (Dummy.exe)
 popd
--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 pushd $SCRIPT_DIR
 cd build/bin
-if [[ "$1" != "test" ]]; then
+if [[ "$1" != "tests" ]]; then
     ./Dummy
 else
     ./DummyTest
