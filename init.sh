b67ab56b1a6ef11be57848a0774a3d2276ef993d
Shaul Fridman <sfridman@synamedia.com>
2021-12-08 21:36:09 +0200

    (~) silent shell prints

--- init.sh
+++ init.sh
@@ -9,21 +9,21 @@ trap 'LAST_COMMAND=$CURRENT_COMMAND; CURRENT_COMMAND=$BASH_COMMAND' DEBUG
 trap 'ERROR_CODE=$?; FAILED_COMMAND=$LAST_COMMAND; tput setaf 1; echo "ERROR: command \"$FAILED_COMMAND\" failed with exit code $ERROR_CODE"; tput sgr0;' ERR INT TERM
 
 if [[ "$1" = "--help" ]]; then
-    echo Usage: ./init.sh
-    echo    Initalize the project.
-    echo    Call this first thing, as its recreate the git repo.
-    exit 0
+  echo Usage: ./init.sh
+  echo Initalize the project.
+  echo Call this first thing, as its recreate the git repo.
+  exit 0
 fi
 
-SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 
-pushd $SCRIPT_DIR
+pushd $SCRIPT_DIR >/dev/null 2>&1
 
 # project name is eather pwd or the first param
 ProjectName=$([ -z "$1" ] && echo $(basename $(pwd)) || echo $1)
 
 FindFiles() {
-    git ls-files | grep -v $0
+  git ls-files | grep -v $0
 }
 
 # clean, just in case
@@ -38,14 +38,14 @@ FindFiles $0 | xargs sed -i -e "s/dummy/$ProjectNameAsIdentifier/g"
 # Change file names from dummy.cpp --> <ProjectNameAsIdentifier>.cpp
 FindFiles $0 | grep dummy | sed -e "s/\(\(.*\)dummy\(.*\)\)/\1 \2$ProjectNameAsIdentifier\3/g" | xargs -n 2 git mv
 
-popd
+popd >/dev/null 2>&1
 
 git clean -df
 
 rm -rf .git
 git init
 git add .
-git rm --cached init.* 	# no need for init scripts anymore
+git rm --cached init.* # no need for init scripts anymore
 git commit -m "Seed project files"
 
 # must be the last one, as it deletes this script :-O
