93c10d3b97b434b467f68aeaa284039b104e183d
Shaul Fridman <shaul.fridman@gmail.com>
2022-01-29 22:19:49 +0200

    (+) update conan.cmake to enable use of vs2022

--- env/conan_build_tool.cmake
+++ env/conan_build_tool.cmake
@@ -6,13 +6,13 @@ if(NOT CONAN_CMD AND CONAN_REQUIRED)
                           COMMAND_ERROR_IS_FATAL ANY)
 endif()
 
-# Download conan.cmake, download tagged release - v0.16.1
+# Download conan.cmake (latest)
 if(NOT EXISTS "${CMAKE_CURRENT_LIST_DIR}/conan.cmake")
   message(
     STATUS
       "Downloading conan.cmake from https://github.com/conan-io/cmake-conan")
   file(DOWNLOAD
-       "https://github.com/conan-io/cmake-conan/raw/v0.16.1/conan.cmake"
+       "https://raw.githubusercontent.com/conan-io/cmake-conan/master/conan.cmake"
        "${CMAKE_CURRENT_LIST_DIR}/conan.cmake")
 endif()
 include(${CMAKE_CURRENT_LIST_DIR}/conan.cmake)
